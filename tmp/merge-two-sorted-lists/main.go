2a480c3f60888094ac2a49c2e6ebd1f2722f9ba4
yljphp <yljphp@gmail.com>
2020-03-05 13:17:26 +0800

    add distribute-candies-to-people

--- tmp/merge-two-sorted-lists/main.go
+++ tmp/merge-two-sorted-lists/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+/**
+21. 合并两个有序链表
+将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+示例：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+
+@see https://leetcode-cn.com/problems/merge-two-sorted-lists
+ */
 func main() {
 
 	l1 := &ListNode{
